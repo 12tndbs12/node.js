7aff8d24c533a8e54e45b009d709bef12e0dd319
SY <12tndbs12@naver.com>
2021-03-13 23:02:04 +0900

    readline 사용하기

--- ch14/node-cli/index.js
+++ ch14/node-cli/index.js
@@ -1,3 +1,23 @@
 #!/usr/bin/env node
+const readline = require('readline');
 
-console.log('Hello CLI', process.argv);
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+console.clear();
+const answerCallback = (answer) => {
+    if (answer === 'y') {
+        console.log('감사');
+        rl.close();
+    } else if (answer === 'n'){
+        console.log('죄송');
+        rl.close();
+    }else {
+        console.log('y나 n만 입력하세요.');
+        rl.question('예제가 재미있습니까? (y/n)', answerCallback);
+    }
+};
+
+rl.question('예제가 재미있습니까? (y/n)', answerCallback);
